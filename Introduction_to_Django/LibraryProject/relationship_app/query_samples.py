ec2a7a5769d15b5e7762b5702a17f12b86737cbd
Brianojunga <brianojunga3@gmail.com>
2025-11-08 23:44:58 +0300

    Add relationship app

---
+++ Introduction_to_Django/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,31 @@
+from .models import Author, Book, Library, Librarian
+
+
+#Query all books by a specific author
+
+# Option 1: Using Author instance
+author = Author.objects.get(name="J.K. Rowling")
+books_by_author = author.books.all()  # thanks to related_name='books'
+
+# Option 2: Using Book filter directly
+books_by_author = Book.objects.filter(author__name="J.K. Rowling")
+
+# Print results
+for book in books_by_author:
+    print(book.title)
+
+
+#List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()  # ManyToManyField reverse
+
+for book in books_in_library:
+    print(book.title)
+
+
+#Retrieve the librarian for a library
+library = Library.objects.get(name="Central Library")
+librarian = library.librarian  # thanks to related_name='librarian'
+
+print(librarian.name)
+
