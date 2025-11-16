c3ace55090d4f2de95cb9d479bd7dfea60c8de64
Brianojunga <brianojunga3@gmail.com>
2025-11-16 18:54:36 +0300

    import in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # Create your views here.
 
@@ -44,3 +45,16 @@ def delete_article(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book.delete()
     return redirect("book_list")
+
+
+# bookshelf/views.py
+def create_book(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/create.html', {'form': form})
+
