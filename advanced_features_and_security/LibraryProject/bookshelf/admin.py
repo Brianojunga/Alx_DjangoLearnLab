261cbc4ffa05823d07b6f687f15979250da9760e
Brianojunga <brianojunga3@gmail.com>
2025-11-16 17:29:40 +0300

    Add custom user in the admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+
+from .models import CustomUser  # your custom user model
 
 # Register your models here.
 @admin.register(Book)
@@ -7,3 +11,57 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ['publication_year']
+
+
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    """Form for editing users in the admin."""
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    """Form for creating users in the admin."""
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = (
+        "user_id",
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("email",)}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+        ("Identifiers", {"fields": ("user_id",)}),  # Your custom field
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("email",)
