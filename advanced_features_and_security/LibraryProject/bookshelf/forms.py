bc1ee4509ceda9decca99bb18421b0e5eec80baa
Brianojunga <brianojunga3@gmail.com>
2025-11-16 18:51:26 +0300

    Example form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,9 @@
 #Exampleforms
+# LibraryProject/bookshelf/forms.py
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'content']  # include the fields you want in the form
