3377e33f38f713a85f872ba23071dd99923a4cb6
Brianojunga <brianojunga3@gmail.com>
2025-11-30 19:03:57 +0300

    Add authentication with views and urls

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
