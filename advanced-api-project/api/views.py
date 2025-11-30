d3a9c5ac9ccb4792ff0d3a3fbcef27c68addfb83
Brianojunga <brianojunga3@gmail.com>
2025-11-30 20:13:02 +0300

    Change the importation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .filters import BookFilter
 
 # Create your views here.
@@ -11,7 +11,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [rest_framework.DjangoFilterBackend]
     filterset_class = BookFilter
 
 class BookDetailView(generics.RetrieveAPIView):
