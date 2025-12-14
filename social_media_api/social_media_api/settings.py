49e9d0711a1a55e12702e6cd7aac32318ccf08e3
Brianojunga <brianojunga3@gmail.com>
2025-12-14 18:18:52 +0300

    Change to use the default rest authentication

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
 }
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
